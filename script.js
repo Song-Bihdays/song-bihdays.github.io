bab75efd840f637b20b9628ef28bdaf9195b8a5f
Song Bihdays <coke-caviar-patchy@duck.com>
2025-01-06 22:09:13 +0700

    Update script.js
    Fixed bug due to deleted image and added log

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("script.js working!");
-
 let PAGES_PER_FETCH = 50;
 let INIT_TIME = Date.now();
 let DATE = new Date();
@@ -9,39 +7,55 @@ let searchString = String(DATE.getDate()).padStart(2, "0") + "/" + months[DATE.g
 
 const $ = q => document.querySelector(q);
 const firstUppercase = string => string[0].toUpperCase() + string.slice(1);
+const pageLog = $("#pageLog");
+pageLog.value = "";
 
 let DATA = [];
 let BIHDAY_OBJ = {};
 let BIHDAY_ARR = [];
 let pageIndex = 0;
 let successCount = 0;
+let imageNames = [];
+
+function log(text=""){
+    console.log(text);
+    pageLog.value += text + "\n";
+    pageLog.scrollTop = pageLog.scrollHeight;
+}
+
+log("====LOG START====")
+log("script.js working!");
 
 function render(arr){
     let htmlString = "";
-    for (let song of arr){
-        htmlString += `<div class="songContainer">${(song[4] == "" ? "" : `<img src="${song[5]}" width="220px">`)}<div class="songData"><h3><a href="https://en.wikipedia.org/wiki/${encodeURIComponent(song[0])}" target="_blank">${song[1]}</a></h3><h4>${song[2]}</h4><p>Birthday: ${song[3]}</p></div></div>`;
+    for (let songId in arr){
+        let song = arr[songId];
+        htmlString += `<div class="songContainer">${(song[4] == "" ? "" : `<img src="${song[5]}" width="220px">`)}<div class="songData"><h3>${+songId+1}. <a href="https://en.wikipedia.org/wiki/${encodeURIComponent(song[0])}" target="_blank">${song[1]}</a></h3><h4>${song[2]}</h4><p>Birthday: ${song[3]}</p></div></div>`;
     }
     $("#songs").innerHTML = htmlString;
 }
 
 function fetchImages(i, MAX){
-    fetch(`https://en.wikipedia.org/w/api.php?action=query&prop=imageinfo&format=json&iiprop=url&iiurlwidth=220&origin=*&titles=${Object.keys(BIHDAY_OBJ).slice(PAGES_PER_FETCH*i, PAGES_PER_FETCH*i+PAGES_PER_FETCH).map(song => "File:" + encodeURIComponent(song)).join("|")}`).then(res => res.json()).then(res => {
+    fetch(`https://en.wikipedia.org/w/api.php?action=query&prop=imageinfo&format=json&iiprop=url&iiurlwidth=220&origin=*&titles=${imageNames.slice(PAGES_PER_FETCH*i, PAGES_PER_FETCH*i+PAGES_PER_FETCH).map(song => "File:" + encodeURIComponent(song)).join("|")}`).then(res => res.json()).then(res => {
         for (let obj of Object.values(res["query"]["pages"])){
             if (obj["imageinfo"] !== undefined){
                 if (obj["imageinfo"][0]["thumburl"] !== undefined){
                     successCount++;
                     BIHDAY_OBJ[firstUppercase(obj["title"].slice(5).replaceAll(" ", "_"))][4] = obj["imageinfo"][0]["thumburl"];
                 }
+            }else if (obj["missing"] !== undefined){
+                BIHDAY_ARR.push([...BIHDAY_OBJ[firstUppercase(obj["title"].slice(5).replaceAll(" ", "_"))], ""]);
+                delete BIHDAY_OBJ[firstUppercase(obj["title"].slice(5).replaceAll(" ", "_"))];
             }
         }
         if (PAGES_PER_FETCH*i + PAGES_PER_FETCH <= MAX){
             pageIndex++;
             fetchImages(pageIndex, MAX);
-            console.log(`${i * PAGES_PER_FETCH} pages thus far!`);
+            log(`${i * PAGES_PER_FETCH} pages thus far!`);
         }else{
-            console.log("DONE fetching images!");
             pageIndex = 0;
             if (successCount == Object.keys(BIHDAY_OBJ).length){
+                log("DONE fetching images!");
                 let i = BIHDAY_ARR.length;
                 for (let song of Object.keys(BIHDAY_OBJ)){
                     BIHDAY_ARR.push(BIHDAY_OBJ[song]);
@@ -54,10 +68,12 @@ function fetchImages(i, MAX){
                     BIHDAY_OBJ[song[0].replaceAll(" ", "_")] = song.slice(1);
                 }
                 BIHDAY_ARR = [];
-                fetchViews(0, BIHDAY_ARR.length-1);
+                log();
+                log("Fetching pageviews...")
+                fetchViews(0, BIHDAY_OBJ.length-1);
             }else{
-                console.log(`${successCount}/${Object.keys(BIHDAY_OBJ).length} images fetched.`);
-                console.log("Refetching images...");
+                log(`${successCount}/${Object.keys(BIHDAY_OBJ).length} images fetched.`);
+                log("Refetching images...");
                 successCount = 0;
                 fetchImages(0, Object.keys(BIHDAY_OBJ).length-1);
             }
@@ -73,26 +89,25 @@ function fetchViews(i, MAX){
                 successCount++;
                 totalViews = Object.values(obj["pageviews"]).reduce((partialSum, a) => partialSum + (a === null ? 0 : a), 0);
             }
-            //console.log(obj, obj["title"].replaceAll(" ", "_"));
             BIHDAY_OBJ[obj["title"].replaceAll(" ", "_")][5] = totalViews;
         }
-        //console.log(`SUCCESS: ${successCount}/${PAGES_PER_FETCH}`);
         if (PAGES_PER_FETCH*i + PAGES_PER_FETCH <= MAX){
             pageIndex++;
             fetchViews(pageIndex, MAX);
-            console.log(`${i * PAGES_PER_FETCH} pages thus far!`);
+            log(`${i * PAGES_PER_FETCH} pages thus far!`);
         }else{
-            console.log("DONE fetching pageviews!");;
+            log(MAX);
+            log(PAGES_PER_FETCH*i + PAGES_PER_FETCH);
             if (successCount == Object.keys(BIHDAY_OBJ).length){
+                log("DONE fetching pageviews!");
                 for (let song of Object.keys(BIHDAY_OBJ)){
                     BIHDAY_ARR.push([song, ...BIHDAY_OBJ[song]]);
                 }
                 BIHDAY_ARR = BIHDAY_ARR.sort((a, b) => b[6] - a[6]);
-                console.log(BIHDAY_ARR);
                 render(BIHDAY_ARR);
             }else{
-                console.log(`${successCount}/${Object.keys(BIHDAY_OBJ).length} pageviews fetched.`);
-                console.log("Refetching pageviews...");
+                log(`${successCount}/${Object.keys(BIHDAY_OBJ).length} pageviews fetched.`);
+                log("Refetching pageviews...");
                 pageIndex = 0;
                 successCount = 0;
                 fetchViews(0, Object.keys(BIHDAY_OBJ).length-1);
@@ -102,11 +117,12 @@ function fetchViews(i, MAX){
 }
 
 if (localStorage.getItem("SONG_DATA") === null || localStorage.getItem("version") === null){
+    log("Fetching cropped data... may take a while");
     fetch("/txt/data/song-data-cropped.txt").then(res => res.text()).then(text => {
-        console.log("Fetched cropped data!");
+        log("Fetched cropped data!");
         localStorage.setItem("SONG_DATA", LZString.compress(text));
         localStorage.setItem("version", 1);
-        console.log("localStorage set up!");
+        log("localStorage set up!");
         DATA = text.split("\r\n");
         DATA.pop();
         for (let bihdayData of DATA.filter(data => data.includes(searchString)).map(data => data.split("|"))){
@@ -116,13 +132,15 @@ if (localStorage.getItem("SONG_DATA") === null || localStorage.getItem("version"
                 BIHDAY_ARR.push(bihdayData);
             }
         }
-        console.log(DATA.length);
+        log();
+        log("Fetching images...")
+        imageNames = Object.keys(BIHDAY_OBJ);
         fetchImages(0, Object.keys(BIHDAY_OBJ).length-1);
     });
 }else{
     DATA = LZString.decompress(localStorage.getItem("SONG_DATA")).split("\r\n");
     DATA.pop();
-    console.log(DATA.length);
+    log("Fetched localStorage!");
     for (let bihdayData of DATA.filter(data => data.includes(searchString)).map(data => data.split("|"))){
         if (bihdayData[4] != ""){
             BIHDAY_OBJ[firstUppercase(bihdayData[4].replaceAll(" ", "_"))] = bihdayData.slice(0,4);
@@ -130,6 +148,8 @@ if (localStorage.getItem("SONG_DATA") === null || localStorage.getItem("version"
             BIHDAY_ARR.push(bihdayData);
         }
     }
-    console.log(BIHDAY_OBJ);
+    log();
+    log("Fetching images...")
+    imageNames = Object.keys(BIHDAY_OBJ);
     fetchImages(0, Object.keys(BIHDAY_OBJ).length-1);
 }
